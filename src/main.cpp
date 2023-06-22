f9edef2e8a35e6b328708a3fbcfce09ecae4d18c
Medardo-Cuellar <soyriku@gmail.com>
2023-06-22 16:58:08 -0500

    Primera version, se le hace un drivetrain basico con llantas mecano, funciona con el sideways invertido, se cambiara en proxima version

---
+++ src/main.cpp
@@ -0,0 +1,208 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Clawbot Competition Template                              */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Drivetrain           drivetrain    1, 10, D        
+// ClawMotor            motor         3               
+// ArmMotor             motor         8               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+void FrontLeftTest(){
+  FrontLeft.spin(forward);
+  
+  Controller1.Screen.clearLine();
+  Controller1.Screen.setCursor(1, 10);
+  Controller1.Screen.print("FrontLeft");
+  task::sleep(4000);
+  FrontRight.stop(brakeType::brake);
+
+}
+void FrontRightTest(){
+  FrontRight.spin(forward);
+
+  
+  Controller1.Screen.clearLine();
+  Controller1.Screen.setCursor(1, 10);
+  Controller1.Screen.print("FrontRight");
+
+  task::sleep(4000);
+  FrontRight.stop(brakeType::brake);
+}
+void RearLeftTest(){
+  RearLeft.spin(forward);
+
+  Controller1.Screen.clearLine();
+  Controller1.Screen.setCursor(1, 10);
+  Controller1.Screen.print("RearLeft");
+
+  task::sleep(4000);
+  FrontRight.stop(brakeType::brake);
+}
+void RearRightTest(){
+  RearRight.spin(forward);
+
+  
+  Controller1.Screen.clearLine();
+  Controller1.Screen.setCursor(1, 10);
+  Controller1.Screen.print("RearRight");
+
+  task::sleep(4000);
+  RearRight.stop(brakeType::brake);
+}
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+
+FrontLeftTest();
+FrontRightTest();
+RearLeftTest();
+RearRightTest();
+
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+    int deadband = 10;
+    int FrontLeftSpeed = 0;
+    int RearLeftSpeed = 0;
+    int FrontRightSpeed = 0;
+    int RearRightSpeed = 0;
+        
+    int Turn = 0;
+    int Forwarde = 0;
+    int Sideways = 0;
+  
+    Turn = Controller1.Axis1.position(); //turn
+    Forwarde = Controller1.Axis3.position(); //forward
+    Sideways = Controller1.Axis4.position(); //sideways
+
+
+    FrontRightSpeed = Forwarde - Sideways + Turn;
+    FrontLeftSpeed = Forwarde + Sideways - Turn;
+    RearRightSpeed = Forwarde + Sideways + Turn;
+    RearLeftSpeed = Forwarde - Sideways - Turn;
+
+    //deadband check and speed setting
+
+    if (abs(FrontLeftSpeed) < deadband) {
+      // Set the speed to zero.
+      FrontLeft.setVelocity(0, percent);
+    } else {
+      // Set the speed to leftMotorSpeed
+      FrontLeft.setVelocity(FrontLeftSpeed, percent);
+    }
+        if (abs(RearLeftSpeed) < deadband) {
+      // Set the speed to zero.
+      RearLeft.setVelocity(0, percent);
+    } else {
+      // Set the speed to leftMotorSpeed
+      RearLeft.setVelocity(RearLeftSpeed, percent);
+    }
+    
+    if (abs(FrontLeftSpeed) < deadband) {
+      // Set the speed to zero.
+      FrontRight.setVelocity(0, percent);
+    } else {
+      // Set the speed to leftMotorSpeed
+      FrontRight.setVelocity(FrontRightSpeed, percent);
+    }
+    
+    if (abs(FrontLeftSpeed) < deadband) {
+      // Set the speed to zero.
+      RearRight.setVelocity(0, percent);
+    } else {
+      // Set the speed to leftMotorSpeed
+      RearRight.setVelocity(RearRightSpeed, percent);
+    }
+    FrontLeft.spin(forward);
+    RearLeft.spin(forward);
+    FrontRight.spin(forward);
+    RearRight.spin(forward);
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
